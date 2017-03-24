2db11394f2bd0d8da2d11b57a57d630c239a7321
Jeremy Hurley <hurleyjer@yahoo.com>
2017-03-23 23:56:09 -0400

    working app

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Headers, Response, Http } from '@angular/http';
+import  'rxjs/Rx';
 
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
 
     private recipes: Recipe[] = [
       new Recipe("dummy", "blah", "http://vilascounty.uwex.edu/files/2013/12/Recipes-Title.png", [
@@ -29,6 +32,33 @@ export class RecipeService {
       this.recipes.splice(id, 1);
     }
 
-  constructor() { }
+    addRecipe(recipe: Recipe) {
+      this.recipes.push(recipe);
+    }
+
+    editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+      this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+    }
+
+    storeData() {
+      const body = JSON.stringify(this.recipes);
+      const headers = new Headers({
+        'Content-Type': 'application/json'
+      });
+      return this.http.put('firebaseUrl/recipes.json', body, {headers: headers});
+    }
+
+    fetchData() {
+      return this.http.get('firebaseUrl/recipes.json')
+      .map((response: Response) => response.json())
+        .subscribe(
+          (data: Recipe[]) => {
+            this.recipes = data;
+            this.recipesChanged.emit(this.recipes);
+          }
+        )
+    }
+
+  constructor(private http: Http) { }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,8 @@ export class ShoppingListService {
 
   private items: Ingredient[] = [];
 
+  constructor() { }
+
   getItems() {
     return this.items;
   }
@@ -16,6 +18,12 @@ export class ShoppingListService {
     this.items.push(item);
   }
 
-  constructor() { }
+  editItem(oldItem: Ingredient, newItem: Ingredient) {
+    this.items[this.items.indexOf(oldItem)] = newItem;
+  }
+
+  deleteItem(item: Ingredient) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
 
 }
